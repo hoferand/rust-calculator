a2e926d583c9f1f73ced3af7053554ceaa83656f
Andreas Hofer <andreas.hofer@student.tuwien.ac.at>
2023-04-16 10:36:36 +0200

    fix typo in error message

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
 						print_error_position(&input, &pos, &pos);
 					}
 					Error::InvalidOperator(op, start, end) => {
-						eprintln!("Error: Invalid operator `{}` found !", op);
+						eprintln!("Error: Invalid operator `{}` found!", op);
 						print_error_position(&input, &start, &end);
 					}
 					Error::UnexpectedToken(token, start, end) => {
