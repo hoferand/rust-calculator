bea739306059d0edea567381481b3d1c82a798d2
Andreas Hofer <andreas.hofer@student.tuwien.ac.at>
2023-09-03 15:17:56 +0200

    fix comment

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,6 +1,6 @@
 use crate::Error;
 
-/// Used for getting arguments for custom function implementations.
+/// Used for getting arguments for function calls.
 pub trait Arguments {
 	fn get_next_arg(&mut self) -> Result<f32, Error>;
 }
